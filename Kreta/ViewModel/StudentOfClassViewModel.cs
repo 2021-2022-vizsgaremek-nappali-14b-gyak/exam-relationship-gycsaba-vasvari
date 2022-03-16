dbf2271eb28128381d15cad50e401bf9a59aee4e
Csaba Gyuris <gyuris.csaba@vasvari.org>
2022-03-16 14:16:50 +0100

    ViewModel adatokkal

--- Kreta/ViewModel/StudentOfClassViewModel.cs
+++ Kreta/ViewModel/StudentOfClassViewModel.cs
@@ -4,9 +4,29 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using System.Collections.ObjectModel;
+
 namespace Kreta.ViewModel
 {
     public class StudentOfClassViewModel
     {
+        private ObservableCollection<string> classes;
+        private ObservableCollection<string> studentsOfClass;
+
+        public StudentOfClassViewModel()
+        {
+            classes = new ObservableCollection<string>();
+            studentsOfClass = new ObservableCollection<string>();
+        }
+
+        public ObservableCollection<string> Classes
+        {
+            get { return classes;}
+        }
+
+        public ObservableCollection<string> StudentOfClass
+        {
+            get { return studentsOfClass; }
+        }
     }
 }
